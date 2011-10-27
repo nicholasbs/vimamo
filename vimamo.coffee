f8d9dbd1312422670d865bf70d531eb8fdfebf59
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2011-10-27 17:03:44 -0400

    rewrite in coffeescript

---
+++ vimamo.coffee
@@ -0,0 +1,100 @@
+# Add new sites to support here
+sites =
+  "www.facebook.com":
+    selector: ".uiStreamStory"
+    offsetBottom: 30
+    offsetTop: -10
+    selectedClass: "vimamo-fb-selected:visible"
+    searchSelector: ".uiSearchInput input[type=text]"
+    onEnter: -> window.location = $(".vimamo-fb-selected .actorName a").attr("href")
+
+  "tumblr.com":
+    selector: ".post"
+    offsetBottom: 10
+    offsetTop: -10
+    onEnter: -> window.location = $(".vimamo-selected .seeMore").attr("href")
+
+  "news.ycombinator.com":
+    selectedClass: "vimamo-hn-selected"
+    searchSelector: "input[name=q]"
+    getFirst: -> $("table tr table tr td.title").first().closest("tr")
+    getNext: (current) ->
+      next = current.next("tr")
+      while next.length > 0 and next.find("td").length < 1
+        next = next.next("tr")
+      next.first()
+    getPrev: (current) ->
+      prev = current.prev "tr"
+      while prev.length > 0 and prev.find("td").length < 1
+        prev = prev.prev "tr"
+      prev.first()
+    onEnter: ->
+      selected = $(".vimamo-hn-selected")
+      if selected.find("td.subtext").length > 0 # submitted URL
+        window.location = selected.find("td a:last-child").attr("href")
+      else # comments link
+        window.location = selected.find("td:last-child a").attr("href")
+
+# You can override these for specific sites above
+defaultOptions =
+  selectedClass: "vimamo-selected"
+  offsetBottom: 0
+  offsetTop: 0
+  getNext: (current) -> current.nextAll(options.selector).first()
+  getPrev: (current) -> current.prevAll(options.selector).first()
+  getFirst: -> $(options.selector).first()
+
+select = (item) -> item.addClass options.selectedClass
+deselect = (item) -> item.removeClass options.selectedClass
+
+# Used for domains that need to be special-cased (e.g., for working across
+# subdomains
+getPrimaryHostname = (hostname) ->
+  if /^.*\.tumblr\.com.*$/i.test hostname then "tumblr.com" else hostname
+
+hostname = getPrimaryHostname(window.location.hostname)
+options = $.extend(defaultOptions, sites[hostname])
+
+jQuery ->
+  first = options.getFirst()
+  select first if first.length > 0
+
+$(document).jkey("j, k", true, (key) ->
+  current = $("." + options.selectedClass)
+  direction = if key is "j" then "down" else direction = "up"
+
+  if direction is "down"
+    next = options.getNext(current)
+  else # up
+    next = options.getPrev(current)
+
+  windowLeft = pageXOffset
+  windowTop = pageYOffset
+  windowBottom = windowTop + $(window).height()
+
+  if next.length > 0
+    deselect current
+    select next
+
+    elementTop = next.offset().top
+    elementBottom = elementTop + next.height()
+
+    if direction is "down" and elementBottom > windowBottom
+      scrollTo(windowLeft, elementBottom - $(window).height() + options.offsetBottom)
+    else if direction is "up" and elementTop < windowTop
+      scrollTo(windowLeft, elementTop + options.offsetTop)
+  else # last element
+    if direction is "down" then scrollTo(windowLeft, windowBottom) else scrollTo(windowLeft, 0)
+)
+
+if options.onEnter
+  $(document).jkey("enter, return", true, options.onEnter)
+
+if options.searchSelector
+  $(document).jkey("/", ->
+    $(options.searchSelector).focus()
+  )
+  
+  $(options.searchSelector).jkey("escape", ->
+    $(this).blur()
+  )
--- vimamo.js
+++ vimamo.js
@@ -1,140 +1,132 @@
-// Add new sites to support here
-var sites = {
-  "www.facebook.com": {
-    selector: ".uiStreamStory",
-    offsetBottom: 30,
-    offsetTop: -10,
-    selectedClass: "vimamo-fb-selected",
-    searchSelector: ".uiSearchInput input[type=text]",
-    onEnter: function() { location = $(".vimamo-fb-selected .actorName a").attr("href"); }
-  },
-  "tumblr.com": {
-    selector: ".post",
-    onEnter: function() { location = $(".vimamo-selected .seeMore").attr("href"); },
-    offsetBottom: 10,
-    offsetTop: -10
-  },
-  "news.ycombinator.com": {
-    selectedClass: "vimamo-hn-selected",
-    // These functions look crazy because HN has bad markup and few classes.
-    getFirst: function() {
-      return $("table tr table tr td.title").first().closest("tr");
+(function() {
+  var defaultOptions, deselect, getPrimaryHostname, hostname, options, select, sites;
+  sites = {
+    "www.facebook.com": {
+      selector: ".uiStreamStory",
+      offsetBottom: 30,
+      offsetTop: -10,
+      selectedClass: "vimamo-fb-selected:visible",
+      searchSelector: ".uiSearchInput input[type=text]",
+      onEnter: function() {
+        return window.location = $(".vimamo-fb-selected .actorName a").attr("href");
+      }
     },
+    "tumblr.com": {
+      selector: ".post",
+      offsetBottom: 10,
+      offsetTop: -10,
+      onEnter: function() {
+        return window.location = $(".vimamo-selected .seeMore").attr("href");
+      }
+    },
+    "news.ycombinator.com": {
+      selectedClass: "vimamo-hn-selected",
+      searchSelector: "input[name=q]",
+      getFirst: function() {
+        return $("table tr table tr td.title").first().closest("tr");
+      },
+      getNext: function(current) {
+        var next;
+        next = current.next("tr");
+        while (next.length > 0 && next.find("td").length < 1) {
+          next = next.next("tr");
+        }
+        return next.first();
+      },
+      getPrev: function(current) {
+        var prev;
+        prev = current.prev("tr");
+        while (prev.length > 0 && prev.find("td").length < 1) {
+          prev = prev.prev("tr");
+        }
+        return prev.first();
+      },
+      onEnter: function() {
+        var selected;
+        selected = $(".vimamo-hn-selected");
+        if (selected.find("td.subtext").length > 0) {
+          return window.location = selected.find("td a:last-child").attr("href");
+        } else {
+          return window.location = selected.find("td:last-child a").attr("href");
+        }
+      }
+    }
+  };
+  defaultOptions = {
+    selectedClass: "vimamo-selected",
+    offsetBottom: 0,
+    offsetTop: 0,
     getNext: function(current) {
-      var next = current;
-      do {
-        next = next.next("tr");
-      } while (next.length > 0 && next.find("td").length < 1)
-      return next.first();
+      return current.nextAll(options.selector).first();
     },
     getPrev: function(current) {
-      var prev = current;
-      do {
-        prev = prev.prev("tr");
-      } while (prev.length > 0 && prev.find("td").length < 1) // ignore empty tr
-      return prev.first();
+      return current.prevAll(options.selector).first();
     },
-    onEnter: function() {
-      var selected = $(".vimamo-hn-selected")
-      if (selected.find("td.subtext").length > 0) { // submitted URL
-        location = selected.find("td a:last-child").attr("href");
-      } else { // comments link
-        location = selected.find("td:last-child a").attr("href");
-      }
-    },
-    searchSelector: "input[name=q]"
-  }
-};
-
-// You can override these for specific sites above
-var defaultOptions = {
-  selectedClass: "vimamo-selected",
-  offsetBottom: 0,
-  offsetTop: 0,
-  getNext: function(current) {
-    return current.nextAll(options.selector).first();
-  },
-  getPrev: function(current) {
-    return current.prevAll(options.selector).first();
-  },
-  getFirst: function() {
-    return $(options.selector).first();
-  }
-};
-
-var hostname = getPrimaryHostname(location.hostname);
-var options = $.extend(defaultOptions, sites[hostname]);
-
-$(document).ready(function() {
-  var first = options.getFirst();
-  if (first.length > 0) {
-    select(first);
-  }
-});
-
-$(document).jkey("j, k", true, function(key) {
-  var next,
-      current = $("." + options.selectedClass),
-      direction = "up";
-
-  if (key === "j") { // down
-    next = options.getNext(current);
-    direction = "down";
-  } else { // key == "k" up
-    next = options.getPrev(current);
-  }
-
-  var windowLeft = pageXOffset,
-      windowTop = pageYOffset,
-      windowBottom = windowTop + $(window).height();
-
-  if (next.length > 0) {
-    deselect(current);
-    select(next);
-
-    var elementTop = next.offset().top,
-        elementBottom = elementTop + next.height();
-
-    if (direction === "down") {
-      if (elementBottom > windowBottom) {
-        scrollTo(windowLeft, elementBottom - $(window).height() + options.offsetBottom);
-      }
-    } else if (elementTop < windowTop) { // up
-        scrollTo(windowLeft, elementTop + options.offsetTop);
+    getFirst: function() {
+      return $(options.selector).first();
     }
-  } else { // last element
-    direction === "down" ? scrollTo(windowLeft, windowBottom) : scrollTo(windowLeft, 0);
-  }
-});
-
-if (options.onEnter) {
-  $(document).jkey("enter, return", true, options.onEnter);
-}
-
-if (options.searchSelector) {
-  $(document).jkey("/", function() {
-    $(options.searchSelector).focus();
+  };
+  select = function(item) {
+    return item.addClass(options.selectedClass);
+  };
+  deselect = function(item) {
+    return item.removeClass(options.selectedClass);
+  };
+  getPrimaryHostname = function(hostname) {
+    if (/^.*\.tumblr\.com.*$/i.test(hostname)) {
+      return "tumblr.com";
+    } else {
+      return hostname;
+    }
+  };
+  hostname = getPrimaryHostname(window.location.hostname);
+  options = $.extend(defaultOptions, sites[hostname]);
+  jQuery(function() {
+    var first;
+    first = options.getFirst();
+    if (first.length > 0) {
+      return select(first);
+    }
   });
-
-  $(options.searchSelector).jkey("escape", function() {
-    $(this).blur();
+  $(document).jkey("j, k", true, function(key) {
+    var current, direction, elementBottom, elementTop, next, windowBottom, windowLeft, windowTop;
+    current = $("." + options.selectedClass);
+    direction = key === "j" ? "down" : direction = "up";
+    if (direction === "down") {
+      next = options.getNext(current);
+    } else {
+      next = options.getPrev(current);
+    }
+    windowLeft = pageXOffset;
+    windowTop = pageYOffset;
+    windowBottom = windowTop + $(window).height();
+    if (next.length > 0) {
+      deselect(current);
+      select(next);
+      elementTop = next.offset().top;
+      elementBottom = elementTop + next.height();
+      if (direction === "down" && elementBottom > windowBottom) {
+        return scrollTo(windowLeft, elementBottom - $(window).height() + options.offsetBottom);
+      } else if (direction === "up" && elementTop < windowTop) {
+        return scrollTo(windowLeft, elementTop + options.offsetTop);
+      }
+    } else {
+      if (direction === "down") {
+        return scrollTo(windowLeft, windowBottom);
+      } else {
+        return scrollTo(windowLeft, 0);
+      }
+    }
   });
-}
-
-
-function select(item) {
-  item.addClass(options.selectedClass);
-}
-
-function deselect(item) {
-  item.removeClass(options.selectedClass);
-}
-
-// Used for domains that need to be special-cased (e.g., for working across
-// subdomains).
-function getPrimaryHostname(hostname) {
-  if (/^.*\.tumblr\.com.*$/i.test(hostname)) {
-    return "tumblr.com";
+  if (options.onEnter) {
+    $(document).jkey("enter, return", true, options.onEnter);
   }
-  return hostname;
-}
+  if (options.searchSelector) {
+    $(document).jkey("/", function() {
+      return $(options.searchSelector).focus();
+    });
+    $(options.searchSelector).jkey("escape", function() {
+      return $(this).blur();
+    });
+  }
+}).call(this);
